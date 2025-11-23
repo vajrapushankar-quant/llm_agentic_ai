e823f673f2ffd045b7eec01c41e03079feb10834
Girija Shankar Vajrapu <vajrapushankar@gmail.com>
2025-11-23 20:25:24 +0530

    update the output dir

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
